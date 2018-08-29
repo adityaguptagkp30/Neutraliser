f7e7608323b1c29bdbc1a5069c9eb4a2f7293e47
ALOK RANJAN <akrsc1999@gmail.com>
2018-08-29 20:24:01 +0530

    new change after today's update

--- javascript/collector.js
+++ javascript/collector.js
@@ -27,6 +27,8 @@
         clear : function() {
             this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
         },
+        
+        
     }
 
     function component(width, height, img, x, y, type) {  //component is collector.
@@ -50,20 +52,51 @@
             else {
                 ctx.fillStyle = img;
                 ctx.fillRect(this.x, this.y, this.width, this.height);
-            }
-        }
+          }}
+            
+            
         this.newPos = function() {
             this.x += this.speedX;
-
+         }
+                                                                       
+        
+         this.collision = function(charge) {    // collision function of charge & collector
+        var collectorleft = this.x;
+        var collectorright = this.x + (this.width);
+        var collectortop = this.y;
+        var collectorbottom = this.y + (this.height);
+        var chargeleft = charge.x;
+        var chargeright = charge.x + (charge.width);
+        var chargetop = charge.y;
+        var chargebottom = charge.y + (charge.height);
+        var blast = true;
+        if ((collectorbottom < chargetop) || (collectortop > chargebottom) || (collectorright < chargeleft) || (collectorleft > chargeright)) {
+            blast = false;
         }
+        return blast;
+    }
+        
+
     }
 
     function updateGameArea(){ 
+                                                
+        
+        for (i = 0; i < charges.length; i++) {
+        if (collector.collision(charges[i])) {
+            charges[i].width = 0;
+            charges[i].height =0;
+            }
+            }
+            
+        
+        
+        
 
          gameArea.clear();
 
         gameArea.frame += 1;
-        if (gameArea.frame == 1 || everyinterval(60)) {
+        if (gameArea.frame==1 || everyinterval(20)) {
             if(Math.round(Math.random())==0){
             charges.push(new component(50, 40, "images/-charge.png", Math.random()*700, 0,"image"));   
             }
@@ -74,23 +107,23 @@
 
 
         for (i = 0; i < charges.length; i++) {
-            charges[i].y += 3;
+            charges[i].y += 5;
             charges[i].update();
         }
-
         collector.speedX=0;
         collector.speedY=0;
-        if ( gameArea.key == 37) {if(collector.x >0){collector.speedX = -5; }}
-        if ( gameArea.key == 39) {if(collector.x + collector.width/2 <700){collector.speedX = 5; }}
+        if ( gameArea.key == 37) {if(collector.x >0){collector.speedX = -10; }}
+        if ( gameArea.key == 39) {if(collector.x + collector.width/2 <700){collector.speedX = 10; }}
         collector.newPos();
         collector.update();
 
     }
 
     function everyinterval(n) {
-        if ((gameArea.frame / n) % 1 == 0) {return true;}
+        if (gameArea.frame %n == 0) {return true;}
         return false;
     }
+
 
 
 
